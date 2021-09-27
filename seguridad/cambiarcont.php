18f196daf4fa3e3c00a95e6e5abd110cf670ba30
Erick <xamaringit@gmail.com>
2021-09-27 18:59:13 -0500

    menu_correccion

---
+++ seguridad/cambiarcont.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../dashmenu/panel_menu.php'; 
+
+
+
+?>
+<div class="right_col" role="main"> 
+    <div class="">
+        <div class="clearfix"></div>
+      
+    </div>   
+</div>
+
+<?php require_once '../dashmenu/panel_footer.php'; ?>
+
+
+</body>
+
+</html>
