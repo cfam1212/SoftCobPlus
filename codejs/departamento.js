f3aaa8c71353d0c92f591def9416c15126faa3a1
Erick <xamaringit@gmail.com>
2021-10-12 18:49:02 -0500

    editar2

--- codejs/departamento.js
+++ codejs/departamento.js
@@ -30,50 +30,6 @@ $(document).ready(function(){
       }
     });
 
-    $('#btnSave').click(function(){
-        _depa = $.trim($("#txtDepa").val());        
-
-        if(_depa == '')
-        {                   
-            mensajesalertify("Ingrese Nombre del Departamento..!","W","top-center",5);  
-            return;
-        }
-  
-        $.ajax({
-            url: "../db/depacrud.php",
-            type: "POST",
-            dataType: "json",
-            data: {id:_id, nomdepa:_depa, estado:_estado, opcion:_opcion},
-            success: function(data){                                 
-                _depaid = data[0].Depaid;
-                _nomdepa = data[0].Departamento;
-                _estado = data[0].Estado;
-
-                if(_depaid == 1){
-                    _desactivar = 'disabled="disabled"';
-                }else{
-                    _desactivar = '';
-                }
-
-                _boton = '<td><div class="text-center"><div class="btn-group"><button class="btn btn-outline-info btn-sm ml-3"' +
-                         'id="btnEditar"><i class="fa fa-pencil-square-o"></i></button><button class="btn btn-outline-danger btn-sm ml-3"'+
-                        _desactivar + 'id="btnEliminar"><i class="fa fa-trash-o"></i></button></div></div></td>'   
-
-                if(_opcion == 0){
-                    TableData.row.add([_depaid, _nomdepa, _estado, _boton]).draw();
-                }
-                else{
-                    TableData.row(_fila).data([_depaid, _nomdepa, _estado, _boton]).draw();
-                }  
-              
-                mensajesalertify("Grabado Correctamente..!","S","bottom-center",5);  
-                $("#modalTAREA").modal("hide");               
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    }); 
 
     
     $(document).on("click","#btnEditar",function(e){      
@@ -120,6 +76,13 @@ $(document).ready(function(){
 
         _depa = $.trim($("#txtDepa").val());  
 
+        if(_depa == '')
+        {                   
+            mensajesalertify("Ingrese Nombre del Departamento..!","W","top-center",5);  
+            return;
+        }
+  
+
         if(_opcion == 1){            
             if(_namedepaold != _depa){
                 $.ajax({
@@ -149,6 +112,8 @@ $(document).ready(function(){
     });
 
     function FunGrabar(){
+       
+
         $.ajax({
             url: "../db/depacrud.php",
             type: "POST",
