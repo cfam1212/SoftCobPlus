605764d17d863bb59457f785d781b6344afbe9b1
Erick <erick.alvear7@gmail.com>
2022-06-15 15:30:55 -0500

    cambiosfront2

--- administracion/editcede.php
+++ administracion/editcede.php
@@ -414,6 +414,9 @@ $producto = $resultado->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-content" id="myModalBg">
             <div class="modal-header" id="header">
                 <h5 class="modal-title" id="modalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>   
             </div>
             <form id="formProducto">
                 <div class="modal-body">
@@ -445,6 +448,9 @@ $producto = $resultado->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-content" id="myModalBg">
             <div class="modal-header" id="headercon">
                 <h5 class="modal-title" id="modalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>   
             </div>
             <form id="formEditContacto">
                 <div class="modal-body">
@@ -535,6 +541,9 @@ $producto = $resultado->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-content" id="myModalBg">
             <div class="modal-header" id="headercat">
                 <h5 class="modal-title" id="modalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>   
             </div>
             <form id="formCatalogoEdit">
                 <div class="modal-body">
@@ -568,6 +577,9 @@ $producto = $resultado->fetchAll(PDO::FETCH_ASSOC);
         <div class="modal-content" id="myModalBg">
             <div class="modal-header" id="headercat">
                 <h5 class="modal-title" id="modalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>   
             </div>
             <form id="formEdit">
                 <div class="modal-body">
