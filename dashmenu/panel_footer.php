b2ebca3a269f710dc505b9f97d9264ab8799291d
Erick <erick.alvear7@gmail.com>
2022-06-30 20:13:35 -0500

    asignarcarterav17

--- dashmenu/panel_footer.php
+++ dashmenu/panel_footer.php
@@ -1,4 +1,33 @@
 <footer>
+   <div id="time">
+        <div class="circle" style="--clr:#3e29ff;">
+            <!-- <div class="dots hr_dot"></div> -->
+            <svg>
+                <circle cx="40" cy="40" r="40"></circle>
+                <circle cx="40" cy="40" r="40" id="hh"></circle>
+            </svg>
+            <div id="hours">00</div>
+        </div>
+        <div class="circle"  style="--clr:#fee800;">
+            <!-- <div class="dots min_dot"></div> -->
+            <svg>
+                <circle cx="40" cy="40" r="40"></circle>
+                <circle cx="40" cy="40" r="40" id="mm"></circle>
+            </svg>
+            <div id="minutes">00</div>
+        </div>
+        <div class="circle"  style="--clr:#04fc43;">
+            <!-- <div class="dots sec_dot"></div> -->
+            <svg>
+                <circle cx="40" cy="40" r="40"></circle>
+                <circle cx="40" cy="40" r="40" id="ss"></circle>
+            </svg>
+            <div id="seconds">00</div>
+        </div>
+        <div class="ap">
+            <div id="ampm">AM</div>
+        </div>
+    </div> 
             <div class="pull-right">
               El viaje digital tu marca, empieza con nostros <a href="https://www.bbplus-ec.com">Best Business Plus</a>
             </div>
@@ -71,4 +100,6 @@
     <script src="../codejs/funciones.js"></script>
 
     <script src="../js/custom.min.js"></script>
+
+    <script src="../jsreloj/script.js"></script>
     
